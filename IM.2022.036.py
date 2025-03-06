d8d768e90dc387b958907ae77449a70be9212745
Ama rathnayake <amarathnayake259@gmail.com>
2025-03-06 15:55:35 +0530

    Updated code

--- IM.2022.036.py
+++ IM.2022.036.py
@@ -4,3 +4,8 @@ def add(a, b):
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 print("Sum:", add(num1, num2))
+
+def subtract(x, y):
+    return x - y
+
+print("Difference:", subtract(num1, num2))
